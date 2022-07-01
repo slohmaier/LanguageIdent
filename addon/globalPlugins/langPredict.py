b8200418a2d7f4678ae1ef6e6c11143e39abab03
Stefan Lohmaier <stefan@slohmaier.de>
2022-07-01 18:09:04 +0200

    fix indentation and func wrapping

--- addon/globalPlugins/langPredict.py
+++ addon/globalPlugins/langPredict.py
@@ -1,19 +1,20 @@
 import globalPluginHandler
 from scriptHandler import script
+from functools import wraps
 from logHandler import log
 import speech
 import fasttext
 from speech.types import SpeechSequence, Optional
 from speech.priorities import Spri
 from speech.commands import (
-	IndexCommand,
-	CharacterModeCommand,
-	LangChangeCommand,
-	BreakCommand,
-	PitchCommand,
-	RateCommand,
-	VolumeCommand,
-	PhonemeCommand,
+    IndexCommand,
+    CharacterModeCommand,
+    LangChangeCommand,
+    BreakCommand,
+    PitchCommand,
+    RateCommand,
+    VolumeCommand,
+    PhonemeCommand,
 )
 
 global synthClass
@@ -23,25 +24,24 @@ synthLangs = []
 
 
 def initLangs():
-    synthClass = str(speech.synthDriverHandler.getSynth().__class__)
-    synthLangs = []
-    for voice in speech.synthDriverHandler.getSynth().availableVoices:
-        lang = voice.language.split('_')[0]
-        if not lang in synthLangs:
-            synthLangs.append(lang)
+	synthClass = str(speech.synthDriverHandler.getSynth().__class__)
+	synthLangs = []
+	for voice in speech.synthDriverHandler.getSynth().availableVoices:
+		lang = voice.language.split('_')[0]
+		if not lang in synthLangs:
+			synthLangs.append(lang)
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
-    def __init__(self):
-        super().__init__()
-        
-        # Wrap speech.speech.speak, so we can get its output first
+	def __init__(self):
+		super().__init__()
+
+		# Wrap speech.speech.speak, so we can get its output first
 		old_speak = speech.speech.speak
 		@wraps(speech.speech.speak)
 		def new_speak(
-				sequence: SpeechSequence,
-				symbolLevel: Optional[int] = None,
-				priority: Spri = Spri.NORMAL
-		):
+			sequence: SpeechSequence,
+			symbolLevel: Optional[int] = None,
+			priority: Spri = Spri.NORMAL):
 			log.debug('LANGPREDICT')
 			return old_speak(sequence, symbolLevel, priority)
 		speech.speech.speak = new_speak
