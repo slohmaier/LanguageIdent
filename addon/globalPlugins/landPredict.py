7c93e8978024f77ca336397abf14d722c489c0af
Stefan Lohmaier <stefan@slohmaier.de>
2022-07-01 14:28:29 +0200

    basic functionality to test globalPlugin infrastructure

--- addon/globalPlugins/landPredict.py
+++ addon/globalPlugins/landPredict.py
@@ -1,9 +1,40 @@
 import globalPluginHandler
 from scriptHandler import script
+from logHandler import log
 import speech
 import fasttext
 
-class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+from speech.commands import (
+	IndexCommand,
+	CharacterModeCommand,
+	LangChangeCommand,
+	BreakCommand,
+	PitchCommand,
+	RateCommand,
+	VolumeCommand,
+	PhonemeCommand,
+)
 
+global synthClass
+synthClass = None
+global synthLangs
+synthLangs = []
+
+
+def initLangs():
+    synthClass = str(speech.synthDriverHandler.getSynth().__class__)
+    synthLangs = []
+    for voice in speech.synthDriverHandler.getSynth().availableVoices:
+        lang = voice.language.split('_')[0]
+        if not lang in synthLangs:
+            synthLangs.append(lang)
+
+def speak(speechSequence: list, *args):
+    log.debug('LANGPREDICIT: speechSequence={0}'.format(str(speechSequence)))
+    speech.___speak(speechSequence, *args)
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	def __init__(self, *args):
         super().__init__(&args)
+        speech.___speak = speech.speak
+        speech.speak = speech.___speak
