f43e0577bc54d85bd2801de94d1b3a2dcbd726cb
Stefan Lohmaier <stefan@slohmaier.de>
2022-07-01 22:29:24 +0200

    reworked speak overriding. speech.speech.speak and speak() from current synth is overriaden. This works with Onecore and IBMTTS

--- addon/globalPlugins/langPredict.py
+++ addon/globalPlugins/langPredict.py
@@ -1,4 +1,3 @@
-from numpy import void0
 import globalPluginHandler
 import sys
 import os
@@ -6,7 +5,7 @@ from scriptHandler import script
 from functools import wraps
 from logHandler import log
 import speech
-import fasttext
+
 from speech.types import SpeechSequence, Optional
 from speech.priorities import Spri
 from speech.commands import (
@@ -43,6 +42,51 @@ def checkSynth():
 			lang = voice.language.split('_')[0]
 			if not lang in synthLangs:
 				synthLangs[lang] = voice.language
+		log.info('LANGPREDICT:\nFound voices:\n'+
+			'\n'.join(
+				['- {0}: {1}'.format(key, synthLangs[key]) for key in synthLangs]
+			)
+		)
+
+		# Wrap speech.speech.speak, so we can get its output first
+		old_synth_speak = speech.synthDriverHandler.getSynth().speak
+		@wraps(speech.synthDriverHandler.getSynth().speak)
+		def new_synth_speak(speechSequence: SpeechSequence):
+			speechSequence = fixSpeechSequence(speechSequence)
+			log.debug('langPredict.synth.speak: '+str(speechSequence))
+			return old_synth_speak(speechSequence)
+		#replace built in speak function
+		speech.synthDriverHandler.getSynth().speak = new_synth_speak
+
+def fixSpeechSequence(speechSequence: SpeechSequence):
+	checkSynth()
+
+	global fastTextModel
+	global synthLangs
+	
+	#deconstruct speechsequence
+	langChangeCmd = None
+	insertLangChangeCmd = None
+	text = ''
+	for item in speechSequence:
+		if type(item) == LangChangeCommand:
+			langChangeCmd = item
+		elif type(item) == str:
+			text += item
+		else:
+			if langChangeCmd is None:
+				insertLangChangeCmd = predictLang(langChangeCmd, text)
+			else:
+				predictLang(langChangeCmd, text)
+			text = ''
+	if text and not langChangeCmd is None:
+		if langChangeCmd is None:
+			insertLangChangeCmd = predictLang(langChangeCmd, text)
+		else:
+			predictLang(langChangeCmd, text)
+	if not insertLangChangeCmd is None:
+		speechSequence.insert(0, insertLangChangeCmd)
+	return speechSequence
 
 def predictLang(langChangeCmd: LangChangeCommand, text: str):
 	global fastTextModel
@@ -52,13 +96,13 @@ def predictLang(langChangeCmd: LangChangeCommand, text: str):
 	if langChangeCmd is None:
 		langChangeCmd = LangChangeCommand(defaultLang)
 	predictedLang = fastTextModel.predict(text)[0][0][9:] #strip '__label__'
+	log.debug('PREDICTED={0} TEXT={1}'.format(str(predictedLang), text))
 
 	#don't use a different dialect due to sorting
 	if defaultLang.startswith(predictedLang):
 		langChangeCmd.lang = defaultLang
 	else:
 		langChangeCmd.lang = synthLangs.get(predictedLang, defaultLang)
-		log.debug('LANGPREDICT: Switching to '+str(predictedLang))
 	return langChangeCmd
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
@@ -78,34 +122,7 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			speechSequence: SpeechSequence,
 			symbolLevel: Optional[int] = None,
 			priority: Spri = Spri.NORMAL):
-			checkSynth()
-
-			global fastTextModel
-			global synthLangs
-			
-			#deconstruct speechsequence
-			langChangeCmd = None
-			insertLangChangeCmd = None
-			text = ''
-			for item in speechSequence:
-				if type(item) == LangChangeCommand:
-					langChangeCmd = item
-				elif type(item) == str:
-					text += item
-				else:
-					if langChangeCmd is None:
-						insertLangChangeCmd = predictLang(langChangeCmd, text)
-					else:
-						predictLang(langChangeCmd, text)
-					text = ''
-			if text and not langChangeCmd is None:
-				if langChangeCmd is None:
-					insertLangChangeCmd = predictLang(langChangeCmd, text)
-				else:
-					predictLang(langChangeCmd, text)
-			if not insertLangChangeCmd is None:
-				speechSequence.insert(0, insertLangChangeCmd)
-
+			speechSequence = fixSpeechSequence(speechSequence)
+			log.debug('langPredict.speech.speak: '+str(speechSequence))
 			return old_speak(speechSequence, symbolLevel, priority)
-		#replace built in speak function
 		speech.speech.speak = new_speak
