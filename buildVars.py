3f1c2afc49f561648e0680ce9bbca699055cf3c8
Stefan Lohmaier <stefan@slohmaier.de>
2022-10-11 19:38:25 +0200

    start renaming to LangIdent

--- buildVars.py
+++ buildVars.py
@@ -16,16 +16,16 @@ def _(arg):
 # Add-on information variables
 addon_info = {
 	# add-on Name/identifier, internal for NVDA
-	"addon_name": "langPredict",
+	"addon_name": "LanguageIdent",
 	# Add-on summary, usually the user visible name of the addon.
 	# Translators: Summary for this add-on
 	# to be shown on installation and add-on information found in Add-ons Manager.
-	"addon_summary": _("Language Prediction for spoken text"),
+	"addon_summary": _("Identify the language for every spoken text using AI and change the language."),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description": _("""Predict the language of every spoken text with an ai model and change the voice accordingly, if available."""),
+	"addon_description": _("""Identify the language for every spoken text using AI and change the language. You can specify a whitelist. Only languages in the whitelist are considered for language identification."""),
 	# version
-	"addon_version": "0.0.2",
+	"addon_version": "0.1.0",
 	# Author(s)
 	"addon_author": "Stefan Lohmaier <stefan@slohmaier.de>",
 	# URL for the add-on documentation support
@@ -33,13 +33,13 @@ addon_info = {
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": None,
+	"addon_minimumNVDAVersion": "2021.3.0",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": '2022.3.0',
+	"addon_lastTestedNVDAVersion": '2022.4.0',
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
-	"addon_updateChannel": None,
+	"addon_updateChannel": "dev",
 }
 
 # Define the python files that are the sources of your add-on.
